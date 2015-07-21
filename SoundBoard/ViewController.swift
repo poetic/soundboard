5d59b76ef7e92954f497df4726142f694d679f49
Matthew Hager <matthew@poeticsystems.com>
2015-07-21 18:10:17 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Matthew Hager on 7/21/15.
+//  Copyright (c) 2015 Poetic Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
